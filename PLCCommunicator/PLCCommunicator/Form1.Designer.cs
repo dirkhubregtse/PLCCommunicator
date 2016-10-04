630d451ed81d118c79dcf21bdf09ca05de473a00
Dirk Hubregtse <dirkhubregtse@gmail.com>
2016-10-04 08:43:00 +0200

    Add files via upload

---
+++ PLCCommunicator/PLCCommunicator/Form1.Designer.cs
@@ -0,0 +1,226 @@
+﻿namespace PLCCommunicator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.IP_address = new System.Windows.Forms.TextBox();
+            this.Set_IP = new System.Windows.Forms.Button();
+            this.Start = new System.Windows.Forms.Button();
+            this.Stop = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.Output_off = new System.Windows.Forms.Button();
+            this.Output1_on = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(231, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(51, 50);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // IP_address
+            // 
+            this.IP_address.Location = new System.Drawing.Point(6, 19);
+            this.IP_address.Name = "IP_address";
+            this.IP_address.Size = new System.Drawing.Size(100, 20);
+            this.IP_address.TabIndex = 1;
+            this.IP_address.TextChanged += new System.EventHandler(this.IP_address_TextChanged);
+            this.IP_address.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.IP_address_KeyPress);
+            // 
+            // Set_IP
+            // 
+            this.Set_IP.Location = new System.Drawing.Point(112, 19);
+            this.Set_IP.Name = "Set_IP";
+            this.Set_IP.Size = new System.Drawing.Size(75, 20);
+            this.Set_IP.TabIndex = 2;
+            this.Set_IP.Text = "Set IP";
+            this.Set_IP.UseVisualStyleBackColor = true;
+            this.Set_IP.Click += new System.EventHandler(this.Set_IP_Click);
+            // 
+            // Start
+            // 
+            this.Start.Location = new System.Drawing.Point(6, 19);
+            this.Start.Name = "Start";
+            this.Start.Size = new System.Drawing.Size(75, 23);
+            this.Start.TabIndex = 3;
+            this.Start.Text = "Start";
+            this.Start.UseVisualStyleBackColor = true;
+            this.Start.Click += new System.EventHandler(this.Start_Click);
+            // 
+            // Stop
+            // 
+            this.Stop.Location = new System.Drawing.Point(112, 19);
+            this.Stop.Name = "Stop";
+            this.Stop.Size = new System.Drawing.Size(75, 23);
+            this.Stop.TabIndex = 4;
+            this.Stop.Text = "Stop";
+            this.Stop.UseVisualStyleBackColor = true;
+            this.Stop.Click += new System.EventHandler(this.Stop_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.Start);
+            this.groupBox1.Controls.Add(this.Stop);
+            this.groupBox1.Location = new System.Drawing.Point(12, 94);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 57);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "PC status";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.IP_address);
+            this.groupBox2.Controls.Add(this.Set_IP);
+            this.groupBox2.Location = new System.Drawing.Point(12, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(200, 76);
+            this.groupBox2.TabIndex = 6;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Connect";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(60, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "\"nowhere\"";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Send to:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.Output_off);
+            this.groupBox3.Controls.Add(this.Output1_on);
+            this.groupBox3.Location = new System.Drawing.Point(12, 180);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(200, 56);
+            this.groupBox3.TabIndex = 7;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Output 0.3";
+            // 
+            // Output_off
+            // 
+            this.Output_off.Location = new System.Drawing.Point(112, 19);
+            this.Output_off.Name = "Output_off";
+            this.Output_off.Size = new System.Drawing.Size(75, 23);
+            this.Output_off.TabIndex = 1;
+            this.Output_off.Text = "Off";
+            this.Output_off.UseVisualStyleBackColor = true;
+            this.Output_off.Click += new System.EventHandler(this.Output_off_Click);
+            // 
+            // Output1_on
+            // 
+            this.Output1_on.Location = new System.Drawing.Point(7, 20);
+            this.Output1_on.Name = "Output1_on";
+            this.Output1_on.Size = new System.Drawing.Size(75, 23);
+            this.Output1_on.TabIndex = 0;
+            this.Output1_on.Text = "On";
+            this.Output1_on.UseVisualStyleBackColor = true;
+            this.Output1_on.Click += new System.EventHandler(this.Output1_on_Click);
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label3.Location = new System.Drawing.Point(130, 251);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(165, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "PLC Communicator v0.1 Kobobss";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(294, 264);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "PLC Communicator";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox IP_address;
+        private System.Windows.Forms.Button Set_IP;
+        private System.Windows.Forms.Button Start;
+        private System.Windows.Forms.Button Stop;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button Output_off;
+        private System.Windows.Forms.Button Output1_on;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ PLCCommunicator/PLCCommunicator/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using S7.Net;
+using System.Net;
+
+namespace PLCCommunicator
+{
+    public partial class Form1 : Form
+    {
+        public string address;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void IP_address_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Set_IP_Click(object sender, EventArgs e)
+        {
+            //Checken of de input een geldig IP-address is
+            IPAddress ipAddress;
+            if (IPAddress.TryParse(IP_address.Text, out ipAddress))
+            {
+                address = IP_address.Text;
+            }
+            else
+            {
+                address = "No valid IP!";
+            }
+            
+            label2.Text = address;
+        }
+
+        private void Start_Click(object sender, EventArgs e)
+        {
+            using (var plc = new Plc(CpuType.S71200, address, 0, 1))
+            {
+                plc.Open();
+                plc.Write("DB1.DBW8.0", 1);
+            }
+        }
+
+        private void Stop_Click(object sender, EventArgs e)
+        {
+            using (var plc = new Plc(CpuType.S71200, address, 0, 1))
+            {
+                plc.Open();
+                plc.Write("DB1.DBW8.0", 0);
+            }
+        }
+
+        private void IP_address_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)ConsoleKey.Enter)
+            {
+                Set_IP_Click(this, new EventArgs());
+            }
+        }
+
+        private void Output1_on_Click(object sender, EventArgs e)
+        {
+            using (var plc = new Plc(CpuType.S71200, address, 0, 1))
+            {
+                plc.Open();
+                plc.Write("DB1.DBW48.0", 1);
+            }
+        }
+
+        private void Output_off_Click(object sender, EventArgs e)
+        {
+            using (var plc = new Plc(CpuType.S71200, address, 0, 1))
+            {
+                plc.Open();
+                plc.Write("DB1.DBW48.0", 0);
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+    }
+}
